b690c520974a45fd73342e65e49b56829bb3b94f
Beza Sirak <sirak.bezawit@gmail.com>
2020-01-28 15:36:29 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,13 +1,15 @@
 def create_an_empty_array
+  array = []
   
 end
 
 def create_an_array
+  array = [ "orange", "yellow", "red", "green"]
   
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array << element
 end
 
 def add_element_to_start_of_array(array, element)
